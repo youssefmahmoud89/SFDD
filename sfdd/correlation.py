e253f5ac65d78e53193ba0d91c98a60c3d4a3cab
yousefma7moud <youssef.youssef@smail.inf.h-brs.de>
2018-08-30 11:37:00 +0200

    correlation library containg pearson correlation only...so far

---
+++ sfdd/correlation.py
@@ -0,0 +1,17 @@
+'''
+This class takes half the window matrix including the previous readings
+of components (0...n) and finds the correlation between every component
+with all the other components in the matrix. 
+
+For each component n, a set of correlated sensors is produced.
+
+'''
+import numpy as np
+
+
+class CorrelationLibrary(object):
+
+    def pearson(input_matrix):
+
+        return np.corrcoef(input_matrix.T)
+
