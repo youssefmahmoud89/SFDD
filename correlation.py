aa6b0f46515c709f540e99a71bd727fe9dc1e235
yousefma7moud <youssef.youssef@smail.inf.h-brs.de>
2018-08-30 11:36:12 +0200

    Auto stash before merge of "master" and "origin/master"
    correlation library including only Pearson correlation method...so far

---
+++ correlation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env
+
+'''
+This script takes half the window matrix including the previous readings
+of components (0...n) and finds the correlation between every component
+with all the other components in the matrix. 
+
+For each component n, a set of correlated sensors is produced.
+
+'''
