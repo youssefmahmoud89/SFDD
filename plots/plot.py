574a3475c0dbb5cff30ce5bed694033cb074a237
yousefma7moud <youssef.youssef@smail.inf.h-brs.de>
2018-09-05 09:43:09 +0200

    plotting different data sets

--- plots/plot.py
+++ plots/plot.py
@@ -4,7 +4,8 @@ import numpy as np
 data = np.loadtxt('/Users/YousefMahmoud/SFDD/test_data/22_10_2016__14_46_08.log',delimiter = ';',skiprows=1)
 
 timestep = data[:,0] - data[15,0]
-plt.plot(timestep[15:],data[15:,20])
+plt.plot(timestep[15:],data[15:,19])
+plt.plot(timestep[15:],data[15:,14])
 
 
 #for i in range(0,len(timestep)):
